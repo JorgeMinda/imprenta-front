b000caab482d8bcad4d61f8654383a0cc5896c0b
Jorge Leonardo Minda <jls.minda@yavirac.edu.ec>
2026-03-23 21:53:02 -0500

    Optimización de build: manualChunks y corrección de seguridad

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,22 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), 
+  plugins: [
+    react(), 
     tailwindcss()
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        // Esta función divide automáticamente los paquetes de node_modules
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    // Opcional: aumenta el límite de la advertencia si sabes que tu App es pesada
+    chunkSizeWarningLimit: 1000, 
+  },
 })
